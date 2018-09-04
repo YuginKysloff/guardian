445b7db5c218c0a466a9ae69a75e753ca3caadec
rennokki <rennokki@gmail.com>
2018-09-04 19:12:46 +0300

    Support for Laravel 5.7 (#20)
    * Using $guarded.
    
    * Using laravel/framework 5.7.x

--- src/Models/PermissionModel.php
+++ src/Models/PermissionModel.php
@@ -7,12 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PermissionModel extends Model
 {
     protected $table = 'permissions';
-    protected $fillable = [
-        'model_id', 'model_type',
-        'target_id', 'target_type',
-        'permission_name',
-        'is_prohibited',
-    ];
+    protected $guarded = [];
     protected $casts = [
         'is_prohibited' => 'bool',
     ];
